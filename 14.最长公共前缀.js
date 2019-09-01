2a990bea314a38e78dd9bd1bebe1e383d0c4650a
[mengj.jin] <[mengj.jin@qunar.com]>
2019-09-01 15:51:40 +0800

    9.1 reduce解决最长公共前缀

--- 14.最长公共前缀.js
+++ 14.最长公共前缀.js
@@ -6,18 +6,31 @@
 /**
  * @param {string[]} strs
  * @return {string}
+ * 9.1;
  */
+// var longestCommonPrefix = function(strs) {
+//     // 在没有初始值的空数组上调用reduce将报错
+//     if(strs.length === 0) return '';
+//     // reduce不指定初始值,commonStr 为第一个元素,item第二个
+//     return strs.reduce((commonStr, item) => {
+//         let i = 0;
+//         while(commonStr[i] && item[i] && commonStr[i] === item[i]) i++;
+//         return commonStr.slice(0,i);
+//     })
+// };
+
 var longestCommonPrefix = function(strs) {
-    // 在没有初始值的空数组上调用reduce将报错
-    if(strs.length === 0) return '';
-    // reduce不指定初始值,commonStr 为第一个元素,item第二个
-    return strs.reduce((commonStr, item) => {
+    if (strs.length === 0) return "";
+    return strs.reduce((comStr, item, index) => {
         let i = 0;
-        while(commonStr[i] && item[i] && commonStr[i] === item[i]) i++;
-        return commonStr.slice(0,i);
-    })
+        // console.log("comStr", comStr);
+        while (
+            i < comStr.length &&
+            i < item.length &&
+            comStr.charAt(i) === item.charAt(i) 
+        ) i++;
+       return comStr.slice(0, i);
+    });
 };
 
-longestCommonPrefix(['aabc', 'aab', 'ab'])
-
-
+longestCommonPrefix(["flower","flow","flight"]);
